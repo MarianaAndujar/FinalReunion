0d80a0dd0e8f295d863ea600b4b95dc8e13d86d0
Irkam <venusos.smp@gmail.com>
2015-01-22 00:00:07 +0100

    dites-moi, ça chie des paillettes ou bien ?

--- static/js/meeting.js
+++ static/js/meeting.js
@@ -1,30 +1,36 @@
-
+/**
+ * 
+ */
 
 /**
  * 
  * @param {Object} parent
  * @param {Object} window_length
+ * @param {Object} origin_date
  */
-function renderTimelineHoursAdminSelector(parent, window_length, id_prefix){	
+function renderTimelineHoursAdminSelector(parent, window_length, origin_date){
+	var ref_elem = document.getElementById("submit_form");
+		
 	var selector = document.createElement("table");
-	parent.appendChild(selector);
+	parent.insertBefore(selector, ref_elem);
 		
 	var selector_labels_row = document.createElement("tr");
 	selector.appendChild(selector_labels_row);
 	var selector_inputs_row = document.createElement("tr");
 	selector.appendChild(selector_inputs_row);
 	
+	var id_prefix = origin_date.name;
 	for(i = 0 ; i < 24 ; i++){
-		selector_label_cell = document.createElement("td");
+		var selector_label_cell = document.createElement("td");
 		selector_labels_row.appendChild(selector_label_cell);
-		selector_label = document.createElement("label");
+		var selector_label = document.createElement("label");
 		selector_label.setAttribute("for", id_prefix+"_hour_"+i);
 		selector_label.textContent = i + ":00 - " + (i + window_length) + ":00";
 		selector_label_cell.appendChild(selector_label);
 		
-		selector_input_cell = document.createElement("td");
+		var selector_input_cell = document.createElement("td");
 		selector_inputs_row.appendChild(selector_input_cell);
-		selector_input = document.createElement("input");
+		var selector_input = document.createElement("input");
 		selector_input.setAttribute("type", "checkbox");
 		selector_input.setAttribute("name", "hours_"+id_prefix);
 		selector_input.setAttribute("value", i);
@@ -32,17 +38,19 @@ function renderTimelineHoursAdminSelector(parent, window_length, id_prefix){
 		selector_input_cell.appendChild(selector_input);
 	}
 	
-	date_input = document.createElement("input");
+	var date_input = document.createElement("input");
 	$(date_input).datepicker();
-	parent.appendChild(date_input);
+	date_input.setAttribute("name", date_input.id);
+	parent.insertBefore(date_input, ref_elem);
 	
-	add_button = document.createElement("button");
+	var add_button = document.createElement("button");
+	add_button.setAttribute("type", "button");
 	add_button.textContent = "ajouter";
 	add_button.onclick = function(){
-		renderTimelineHoursAdminSelector(parent_id, window_length, date_input.innerText);
+		renderTimelineHoursAdminSelector(parent, window_length, date_input);
 		return false;
 	};
-	parent.appendChild(add_button);
+	parent.insertBefore(add_button, ref_elem);
 }
 
 /**
@@ -51,22 +59,45 @@ function renderTimelineHoursAdminSelector(parent, window_length, id_prefix){
  * @param {Object} window_length
  */
 function renderAdminForm(parent_id, window_length){
-	parent = document.getElementById(parent_id);
+	var parent = document.getElementById(parent_id);
 	
-	form = document.createElement("form");
-	parent.appendChild(form);	
+	var form = document.createElement("form");
+	form.setAttribute("method", "post");
+	form.setAttribute("action", "toast.html");
+	parent.appendChild(form);
 	
-	date_input = document.createElement("input");
+	var window_length_input = document.createElement("input");
+	window_length_input.setAttribute("value", window_length);
+	var window_length_button = document.createElement("button");
+	window_length_button.setAttribute("type", "button");
+	window_length_button.textContent = "changer durée (réinitialise le formulaire)";
+	window_length_button.onclick = function(){
+		parent.parentNode.removeChild(parent);
+		renderAdminForm(parent_id, window_length_input.value);
+		return null;
+	};
+	form.appendChild(window_length_input);
+	form.appendChild(window_length_button);
+	form.appendChild(document.createElement("br"));
+	
+	var date_input = document.createElement("input");
 	$(date_input).datepicker();
+	date_input.setAttribute("name", date_input.id);
 	form.appendChild(date_input);
 	
-	add_button = document.createElement("button");
+	var add_button = document.createElement("button");
+	add_button.setAttribute("type", "button");
 	add_button.textContent = "ajouter";
 	add_button.onclick = function(){
-		renderTimelineHoursAdminSelector(form, window_length, date_input.textContent);
+		renderTimelineHoursAdminSelector(form, window_length, date_input);
 		return false;
 	};
 	form.appendChild(add_button);
+	
+	var submit_button = document.createElement("input");
+	submit_button.setAttribute("type", "submit");
+	submit_button.setAttribute("id", "submit_form");
+	form.appendChild(submit_button);
 }
 
 function renderTimelineSelector(parent, datetimes, window_length){
