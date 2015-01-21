3d1602f9c574bd67666b4f1acf6d341762001977
Irkam <venusos.smp@gmail.com>
2015-01-21 23:02:08 +0100

    LEEEET'S DO THE TIME WARP AGAAAIIIIIN

--- static/js/meeting.js
+++ static/js/meeting.js
@@ -1,6 +1,13 @@
-function renderTimelineHoursAdminSelector(parent, window_length){
+
+
+/**
+ * 
+ * @param {Object} parent
+ * @param {Object} window_length
+ */
+function renderTimelineHoursAdminSelector(parent, window_length, id_prefix){	
 	var selector = document.createElement("table");
-	document.getElementById(parent).appendChild(selector);
+	parent.appendChild(selector);
 		
 	var selector_labels_row = document.createElement("tr");
 	selector.appendChild(selector_labels_row);
@@ -11,7 +18,7 @@ function renderTimelineHoursAdminSelector(parent, window_length){
 		selector_label_cell = document.createElement("td");
 		selector_labels_row.appendChild(selector_label_cell);
 		selector_label = document.createElement("label");
-		selector_label.setAttribute("for", "hour_"+i);
+		selector_label.setAttribute("for", id_prefix+"_hour_"+i);
 		selector_label.textContent = i + ":00 - " + (i + window_length) + ":00";
 		selector_label_cell.appendChild(selector_label);
 		
@@ -19,11 +26,47 @@ function renderTimelineHoursAdminSelector(parent, window_length){
 		selector_inputs_row.appendChild(selector_input_cell);
 		selector_input = document.createElement("input");
 		selector_input.setAttribute("type", "checkbox");
-		selector_input.setAttribute("name", "date");
+		selector_input.setAttribute("name", "hours_"+id_prefix);
 		selector_input.setAttribute("value", i);
-		selector_input.setAttribute("id", "hour_"+i);
+		selector_input.setAttribute("id", id_prefix+"_hour_"+i);
 		selector_input_cell.appendChild(selector_input);
 	}
+	
+	date_input = document.createElement("input");
+	$(date_input).datepicker();
+	parent.appendChild(date_input);
+	
+	add_button = document.createElement("button");
+	add_button.textContent = "ajouter";
+	add_button.onclick = function(){
+		renderTimelineHoursAdminSelector(parent_id, window_length, date_input.innerText);
+		return false;
+	};
+	parent.appendChild(add_button);
+}
+
+/**
+ * 
+ * @param {Object} parent_id
+ * @param {Object} window_length
+ */
+function renderAdminForm(parent_id, window_length){
+	parent = document.getElementById(parent_id);
+	
+	form = document.createElement("form");
+	parent.appendChild(form);	
+	
+	date_input = document.createElement("input");
+	$(date_input).datepicker();
+	form.appendChild(date_input);
+	
+	add_button = document.createElement("button");
+	add_button.textContent = "ajouter";
+	add_button.onclick = function(){
+		renderTimelineHoursAdminSelector(form, window_length, date_input.textContent);
+		return false;
+	};
+	form.appendChild(add_button);
 }
 
 function renderTimelineSelector(parent, datetimes, window_length){
@@ -74,7 +117,7 @@ function renderTimelineSelector(parent, datetimes, window_length){
 	});
 }
 
-$(document).ready(renderTimelineHoursAdminSelector("selector-container", 2));
+$(document).ready(renderAdminForm("selector-container", 2));
 
 var dates = [
 	{
