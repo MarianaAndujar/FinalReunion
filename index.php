276c060f41adb0c52d5150b25f497ba9473896f5
Mariana Andujar <mariana.andujarf@gmail.com>
2015-01-21 11:47:41 +0100

    File controlor create

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
 <?php
 	session_start();
+
+	// affichage des vues de l'en tete et du sommaire 
+	include("view/Header.php");	
+	include("view/Menu.php");
+
+	// instantiation de l'acces aux données
+	require_once("model/myPDO.php");
+	$pdo = myPdo::getPdo();
+	
+	// utilisation du controleur adapté
+	$page = $_REQUEST["uc"];
+	
+	
+	// Redirection sur le controleur approprié
+	switch($page)
+	{
+	    case "reunions" :
+	        include("controler/meetings.php");
+	        break;
+	    case "login" :
+	        include("controler/login.php");
+	        break;
+	    case "logout" :
+	        include("controler/logout.php");
+	        break;
+	    case "create" : 
+	        include("controler/create.php");
+	    case "register" :
+	        include("controler/register.php");
+	        break;
+		default:
+				include("view/home.php");
+	
+	}
+	
+	include("view/Footer.php");
+
+
+	
 ?>
